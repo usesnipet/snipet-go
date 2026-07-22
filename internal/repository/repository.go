0c2bb1643514da46ebaa1a43a86f1c8cdf2ee81a
Mayron <mayron.fernandes@alghorit.com.br>
2026-07-22 11:22:23 -0300

    chore: remove log from repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"context"
-	"log"
 
 	apperr "github.com/usesnipet/snipet/internal/app-err"
 	"github.com/usesnipet/snipet/internal/filter"
@@ -30,8 +29,6 @@ func (r *Repository[T]) Filter(ctx context.Context, filterOptions *filter.Option
 	if err != nil {
 		return nil, err
 	}
-
-	log.Println("filterOptions", filterOptions)
 
 	chain, err := filterOptions.ToGorm(gorm.G[T](r.db(ctx)))
 	if err != nil {
