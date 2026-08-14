45801f21394d15c2c67b33c033ab1a53b28993fc
Mayron <mayron.g.fernandes@gmail.com>
2026-08-13 21:28:57 -0300

    fix: update default admin email in user config and reorder auth config fields

--- config/auth.go
+++ config/auth.go
@@ -3,15 +3,16 @@ package config
 import "time"
 
 type AuthConfig struct {
-	JWTSecret              string        `env:"JWT_SECRET, default=change-me-in-production"`
-	JWTExpiration          time.Duration `env:"JWT_EXPIRATION, default=15m"`
-	JWTIssuer              string        `env:"JWT_ISSUER, default=https://snipet.cloud"`
-	JWTAudience            string        `env:"JWT_AUDIENCE, default=https://snipet.cloud"`
-	RefreshTokenExpiration time.Duration `env:"REFRESH_TOKEN_EXPIRATION, default=720h"`
+	JWTSecret     string        `env:"JWT_SECRET, default=change-me-in-production"`
+	JWTExpiration time.Duration `env:"JWT_EXPIRATION, default=15m"`
+	JWTIssuer     string        `env:"JWT_ISSUER, default=https://snipet.cloud"`
+	JWTAudience   string        `env:"JWT_AUDIENCE, default=https://snipet.cloud"`
 
-	AppURL                         string        `env:"APP_URL, default=http://localhost:5173"`
+	RefreshTokenExpiration         time.Duration `env:"REFRESH_TOKEN_EXPIRATION, default=720h"`
 	ActivateAccountTokenExpiration time.Duration `env:"ACTIVATE_ACCOUNT_TOKEN_EXPIRATION, default=24h"`
 	ResetPasswordTokenExpiration   time.Duration `env:"RESET_PASSWORD_TOKEN_EXPIRATION, default=1h"`
+
+	AppURL string `env:"APP_URL, default=http://localhost:5173"`
 
 	GoogleClientID     string `env:"GOOGLE_CLIENT_ID"`
 	GoogleClientSecret string `env:"GOOGLE_CLIENT_SECRET"`
--- config/user.go
+++ config/user.go
@@ -2,6 +2,6 @@ package config
 
 type UserConfig struct {
 	AdminName     string `env:"ADMIN_NAME, default=Admin"`
-	AdminEmail    string `env:"ADMIN_EMAIL, default=admin@admin.com"`
+	AdminEmail    string `env:"ADMIN_EMAIL, default=admin@snipet.com"`
 	AdminPassword string `env:"ADMIN_PASSWORD"`
 }
