509b8794f3bfbcdadc5c59a5812aa31cc684d32b
Mayron <mayron.fernandes@alghorit.com.br>
2026-08-19 16:28:25 -0300

    feat: client with new format

--- internal/model/client.go
+++ internal/model/client.go
@@ -1,27 +1,27 @@
 package model
 
-type ClientConfig struct {
-	OIDC struct {
-		Issuer   string `json:"issuer" validate:"omitempty,url"`
-		Audience string `json:"audience" validate:"omitempty,url"`
-		Enabled  bool   `json:"enabled"`
-	} `json:"oidc"`
-	Webhook struct {
-		URL     string `json:"url" validate:"omitempty,url"`
-		Enabled bool   `json:"enabled"`
-	} `json:"webhook"`
-	Anonymous struct {
-		Enabled bool `json:"enabled"`
-	} `json:"anonymous"`
-}
+type ClientStatus string
+
+const (
+	ClientStatusPending          ClientStatus = "pending"
+	ClientStatusActivating       ClientStatus = "activating"
+	ClientStatusActive           ClientStatus = "active"
+	ClientStatusActivationFailed ClientStatus = "activation_failed"
+	ClientStatusDeactivated      ClientStatus = "deactivated"
+)
 
 type Client struct {
 	ID string `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
 
-	TenantID string       `gorm:"type:uuid;not null;index" json:"tenant_id"`
-	Code     string       `gorm:"type:varchar(10);not null;unique" json:"code"`
-	Name     string       `gorm:"type:varchar(255);not null" json:"name"`
-	Config   ClientConfig `gorm:"type:jsonb;not null;serializer:json" json:"config"`
+	TenantID     string       `gorm:"type:uuid;not null;index" json:"tenant_id"`
+	Name         string       `gorm:"type:varchar(255);not null" json:"name"`
+	Description  string       `gorm:"type:text;not null" json:"description"`
+	Status       ClientStatus `gorm:"type:varchar(255);not null" json:"status"`
+	ErrorMessage string       `gorm:"type:text;not null" json:"error_message"`
+	Provider     string       `gorm:"type:varchar(255);not null" json:"provider"`
+
+	KeyID   string `gorm:"type:varchar(255);not null;unique" json:"key_id"`
+	KeyHash string `gorm:"type:text;not null;unique" json:"-"`
 
 	Tenant        *Tenant              `gorm:"foreignKey:TenantID;references:ID;constraint:OnDelete:CASCADE" json:"tenant"`
 	Sessions      []Session            `gorm:"foreignKey:ClientID;references:ID;constraint:OnDelete:CASCADE" json:"sessions"`
