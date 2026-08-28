8452332c2f7e9d980809c289d6a18bb86ebaccb6
Mayron <mayron.g.fernandes@gmail.com>
2026-08-28 18:03:59 -0300

    feat: add CORS middleware to API router

--- go.mod
+++ go.mod
@@ -69,6 +69,7 @@ require (
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
 	github.com/go-jose/go-jose/v4 v4.1.4 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
--- internal/api/api.go
+++ internal/api/api.go
@@ -7,6 +7,7 @@ import (
 	scalar "github.com/MarceloPetrucio/go-scalar-api-reference"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 type Api struct {
@@ -15,6 +16,15 @@ type Api struct {
 
 func New() *Api {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
